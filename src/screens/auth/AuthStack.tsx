2552eaea0685bb931e3533511eb7ceef563ed624
Andrzej Melcer <andymelcer@wp.pl>
2022-05-28 22:39:22 +0200

    Handle first app launch

--- src/screens/auth/AuthStack.tsx
+++ src/screens/auth/AuthStack.tsx
@@ -1,10 +1,12 @@
 import { ParamListBase, StackNavigationState, TypedNavigator } from '@react-navigation/native'
 import { NativeStackNavigationEventMap, NativeStackNavigationOptions } from '@react-navigation/native-stack'
 import { NativeStackNavigatorProps } from '@react-navigation/native-stack/lib/typescript/src/types'
-import React from 'react'
-import { Text } from 'react-native-paper'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import React, { useEffect, useState } from 'react'
+import { ActivityIndicator, Text } from 'react-native-paper'
 import SignIn from './signIn/SignIn'
 import Register from './register/Register'
+import FirstLaunch from '../firstLaunch/FirstLaunch'
 
 interface Props {
     Stack: TypedNavigator<
@@ -24,6 +26,26 @@ interface Props {
 }
 
 export default function AuthStack({ Stack }: Props) {
+    const [isLoading, setIsLoading] = useState(true)
+    const [firstTimeOpen, setFirsTimeOpen] = useState(true)
+
+    useEffect(() => {
+        AsyncStorage.getItem('firstLaunch')
+            .then((launched) => {
+                if (launched === 'true') {
+                    setFirsTimeOpen(false)
+                }
+            })
+            // eslint-disable-next-line no-console
+            .catch((e) => console.warn(e))
+            .finally(() => {
+                setIsLoading(false)
+                AsyncStorage.setItem('firstLaunch', 'true')
+            })
+    }, [])
+
+    if (isLoading) return <ActivityIndicator animating />
+
     return (
         <>
             <Text>Hello</Text>
@@ -32,6 +54,7 @@ export default function AuthStack({ Stack }: Props) {
                     headerShown: false,
                 }}
             >
+                {firstTimeOpen && <Stack.Screen name="firstLaunch" component={FirstLaunch} />}
                 <Stack.Screen name="SignIn" component={SignIn} />
                 <Stack.Screen name="Register" component={Register} />
             </Stack.Navigator>
